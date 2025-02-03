6d88cc046a522ca62f633df92ce1b550dd20960c
NareshAdhe <157879129+NareshAdhe@users.noreply.github.com>
2025-02-03 09:45:47 +0530

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -46,7 +46,7 @@ const Home = () => {
                 View My Work
               </Link>
 
-              <a
+              <Link
                 href="/Naresh-Resume.pdf"
                 download="Naresh-Resume.pdf"
                 className="overflow-hidden relative bg-[#000] hover:bg-[#111111] font-bold text-white text-lg py-3 px-8 rounded-lg shadow-lg transition-all duration-300 flex items-center gap-2 justify-center"
@@ -73,11 +73,6 @@ const Home = () => {
                     clipRule="evenodd"
                   />
                 </svg>
-                Download Resume
-              </a>e="h-5 w-5 animate-bounce"
-                  viewBox="0 0 20 20"
-                  fill="currentColor"
-                >
                   <path
                     fillRule="evenodd"
                     d="M3 17a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zm3.293-7.707a1 1 0 011.414 0L9 10.586V3a1 1 0 112 0v7.586l1.293-1.293a1 1 0 111.414 1.414l-3 3a1 1 0 01-1.414 0l-3-3a1 1 0 010-1.414z"
