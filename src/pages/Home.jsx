fcadd51b2826275cbb6fea2e58e23111286ad479
NareshAdhe <157879129+NareshAdhe@users.noreply.github.com>
2025-02-03 09:49:29 +0530

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -45,16 +45,6 @@ const Home = () => {
               >
                 View My Work
               </Link>
-
-              <Link
-                href="/Naresh-Resume.pdf"
-                download="Naresh-Resume.pdf"
-                className="overflow-hidden relative bg-[#000] hover:bg-[#111111] font-bold text-white text-lg py-3 px-8 rounded-lg shadow-lg transition-all duration-300 flex items-center gap-2 justify-center"
-              >
-                <div className="spin-border"></div>
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  classNam
               <a
                 href="https://nareshadhe.vercel.app/Naresh-Resume.pdf"
                 download="Naresh-Resume.pdf"
@@ -67,12 +57,6 @@ const Home = () => {
                   viewBox="0 0 20 20"
                   fill="currentColor"
                 >
-                  <path
-                    fillRule="evenodd"
-                    d="M3 17a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zm3.293-7.707a1 1 0 011.414 0L9 10.586V3a1 1 0 112 0v7.586l1.293-1.293a1 1 0 111.414 1.414l-3 3a1 1 0 01-1.414 0l-3-3a1 1 0 010-1.414z"
-                    clipRule="evenodd"
-                  />
-                </svg>
                   <path
                     fillRule="evenodd"
                     d="M3 17a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zm3.293-7.707a1 1 0 011.414 0L9 10.586V3a1 1 0 112 0v7.586l1.293-1.293a1 1 0 111.414 1.414l-3 3a1 1 0 01-1.414 0l-3-3a1 1 0 010-1.414z"
